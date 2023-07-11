c9901d80cfcd9bd3675be8374a948c90e04a7278
Moti Elitzur <42735869+MotiElitzur@users.noreply.github.com>
2023-07-11 14:40:27 +0300

    Add App class

--- app/src/main/java/com/motiapps/melodify/ui/MainActivity.kt
+++ app/src/main/java/com/motiapps/melodify/ui/MainActivity.kt
@@ -1,17 +1,24 @@
-package com.motiapps.melodify
+package com.motiapps.melodify.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.motiapps.melodify.ui.theme.MelodifyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Moti")
                 }
             }
         }
@@ -31,16 +38,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+
+    Column(
         modifier = modifier
-    )
+            .background(Color.Green)
+            .fillMaxSize(),
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MelodifyTheme {
-        Greeting("Android")
+        Greeting("Moti")
     }
 }
