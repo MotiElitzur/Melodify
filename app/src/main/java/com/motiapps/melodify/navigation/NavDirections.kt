c8e71764dd2192fb56ad413a9051769f19edbec0
Moti Elitzur <42735869+MotiElitzur@users.noreply.github.com>
2023-07-31 15:22:03 +0300

    wip

--- app/src/main/java/com/motiapps/melodify/navigation/NavDirections.kt
+++ app/src/main/java/com/motiapps/melodify/navigation/NavDirections.kt
@@ -1,10 +1,14 @@
 package com.motiapps.melodify.navigation
 
-sealed class NavDirections(val route: String) {
-    object Splash : NavDirections("splash")
-    object Login : NavDirections("login")
-    object Register : NavDirections("register")
-    object Home : NavDirections("home")
-    object Profile : NavDirections("profile")
-    object CreateChat : NavDirections("create_chat")
+sealed class NavDirections {
+
+    val route: String
+        get() = this::class.simpleName?.lowercase() ?: ""
+
+    object Splash : NavDirections()
+    object Login : NavDirections()
+    object Register : NavDirections()
+    object Home : NavDirections()
+    object Profile : NavDirections()
+    object CreateChat : NavDirections()
 }
