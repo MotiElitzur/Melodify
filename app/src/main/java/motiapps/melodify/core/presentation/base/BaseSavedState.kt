0e7a49fc9e3a4cd3862d096012e761c729772eb4
Moti Elitzur <42735869+MotiElitzur@users.noreply.github.com>
2024-07-18 18:40:39 +0300

    little fixes

--- app/src/main/java/motiapps/melodify/core/presentation/base/BaseSavedState.kt
+++ app/src/main/java/motiapps/melodify/core/presentation/base/BaseSavedState.kt
@@ -3,5 +3,5 @@ package motiapps.melodify.core.presentation.base
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize // so it can be saved in savedStateHandle
-open class ViewState : Parcelable
+@Parcelize // so it can be saved in savedStateHandle.
+open class BaseSavedState : Parcelable, IViewState
