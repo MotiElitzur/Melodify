43fc390f0f5ed684a1b2eeb9de37c0c14f91b481
Moti Elitzur <42735869+MotiElitzur@users.noreply.github.com>
2025-02-25 17:11:27 +0200

    Update build.gradle.kts

--- melodify-permission/build.gradle.kts
+++ melodify-permission/build.gradle.kts
@@ -47,14 +47,14 @@ dependencies {
 
 
 group = "com.github.MotiElitzur"
-version = "1.0.0"
+version = "1.0"
 
 publishing {
     publications {
         create<MavenPublication>("release") {
             groupId = "com.github.MotiElitzur"
             artifactId = "melodify-permission"
-            version = "1.0.0"
+            version = "1.0"
             afterEvaluate {
                 from(components["release"])
             }
